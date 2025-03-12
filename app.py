f6c1be421ec28407432a5ec6b230b897aa2df969
mattwundo <37961603+mattwundo@users.noreply.github.com>
2025-03-12 14:52:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def predict():
 
     return jsonify(response)
 import os
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Use Render's PORT
+    port = int(os.environ.get("PORT", 10000))  # Render automatically sets $PORT
     app.run(host='0.0.0.0', port=port)
 
